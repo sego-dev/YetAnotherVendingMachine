5525bcbe2392041efeb9473d05f9e784a2ddc621
Sergey Gorlanov <sgorlanov@d-l-s.ru>
2016-06-29 15:21:35 +0300

    add failed test for insert coins

--- YetAnotherVendingMachine.Tests/VendingMachineTest.cs
+++ YetAnotherVendingMachine.Tests/VendingMachineTest.cs
@@ -16,5 +16,30 @@ namespace YetAnotherVendingMachine.Tests
             };
             vendingMachine.InsertCoin(coin);
         }
+
+        [TestMethod]
+        public void ShouldAddToMoneyWhenInsertCoin()
+        {
+            var vendingMachine = new VendingMachine();
+            var coin = new Money()
+            {
+                Cents = 20
+            };
+            var insertedMoney = vendingMachine.InsertCoin(coin);
+            Assert.AreEqual(20, insertedMoney.Cents);
+        }
+
+        [TestMethod]
+        public void ShouldAddToMoneyWhenInsertCoinTwice()
+        {
+            var vendingMachine = new VendingMachine();
+            var coin = new Money()
+            {
+                Cents = 20
+            };
+            vendingMachine.InsertCoin(coin);
+            var insertedMoney = vendingMachine.InsertCoin(coin);
+            Assert.AreEqual(40, insertedMoney.Cents);
+        }
     }
 }
