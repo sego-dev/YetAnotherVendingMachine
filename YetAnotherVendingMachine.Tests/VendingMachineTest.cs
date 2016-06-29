2df038d7350f8484d3eacf71702b0b41177ce3d9
Sergey Gorlanov <sgorlanov@d-l-s.ru>
2016-06-29 14:41:52 +0300

    add failed test for inserted coins
    we can insert only following coins: 5ȼ, 10ȼ, 20ȼ, 50ȼ, 1 € and 2 €

---
+++ YetAnotherVendingMachine.Tests/VendingMachineTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YetAnotherVendingMachine.Tests
+{
+    [TestClass]
+    public class VendingMachineTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(WrongCoinsException))]
+        public void ShouldThrowExceptionWhenWrongCoins()
+        {
+            var vendingMachine = new VendingMachine();
+            var coin = new Money()
+            {
+                Cents = 25
+            };
+            vendingMachine.InsertCoin(coin);
+        }
+    }
+}
---
+++ YetAnotherVendingMachine/VendingMachine.cs
@@ -0,0 +1,24 @@
+﻿namespace YetAnotherVendingMachine
+{
+    public class VendingMachine : IVendingMachine
+    {
+        public string Manufacturer { get; }
+        public Money Amount { get; }
+        public Product[] Products { get; set; }
+
+        public Money InsertCoin(Money amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Money ReturnMoney()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Product Buy(int productNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
