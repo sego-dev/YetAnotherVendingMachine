be5a7f4dc351d092800bb277686aaaaf6b88ab71
Sergey Gorlanov <sgorlanov@d-l-s.ru>
2016-06-29 15:36:16 +0300

    add implementation of insert coins

--- YetAnotherVendingMachine/VendingMachine.cs
+++ YetAnotherVendingMachine/VendingMachine.cs
@@ -2,17 +2,38 @@
 {
     public class VendingMachine : IVendingMachine
     {
+        public VendingMachine()
+        {
+            Amount = new Money();
+        }
         public string Manufacturer { get; }
-        public Money Amount { get; }
+        public Money Amount { get; private set; }
         public Product[] Products { get; set; }
 
+        /// <summary> Inserts the coin into vending machine. </summary>
+        /// <param name="amount">Coin amount.</param>
+        /// <returns>Amount of inserted money</returns>
         public Money InsertCoin(Money amount)
         {
             if (!CoinValidator.IsValid(amount))
             {
                 throw new WrongCoinsException();
             }
-            return new Money();
+            AddMoney(amount);
+            return Amount;
+        }
+
+        /// <summary>
+        /// add instance money
+        /// </summary>
+        /// <param name="amount"></param>
+        private void AddMoney(Money amount)
+        {
+            Amount = new Money()
+            {
+                Cents = Amount.Cents + amount.Cents,
+                Euros = Amount.Euros + amount.Cents
+            };
         }
 
         public Money ReturnMoney()
