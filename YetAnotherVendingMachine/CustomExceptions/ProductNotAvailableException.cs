f1d4c44ac2c956c3be2f5485b5207011bbd6af57
Sergey Gorlanov <sgorlanov@d-l-s.ru>
2016-06-30 11:43:04 +0300

    FIX: add to git somefiles

---
+++ YetAnotherVendingMachine/CustomExceptions/ProductNotAvailableException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetAnotherVendingMachine
+{
+    class ProductNotAvailableException : InvalidOperationException
+    {
+        public ProductNotAvailableException() : base("Product not available")
+        {
+            
+        }
+    }
+}
---
+++ YetAnotherVendingMachine/CustomExceptions/ProductNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetAnotherVendingMachine
+{
+    class ProductNotFoundException : InvalidOperationException
+    {
+        public ProductNotFoundException() : base("Product not found")
+        {
+            
+        }
+    }
+}
